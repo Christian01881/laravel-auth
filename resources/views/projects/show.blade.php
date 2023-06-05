5698a88dd5618ea1db82a2217d5d79721ae4f84a
Christian01881 <chrispamo01881@gmail.com>
2023-06-05 20:25:10 +0200

    project finished

---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="d-flex">
+    <div class="minus">
+        <img class="w-100" src="{{ $project->image }}" alt="">
+    </div>
+    <div class="major">
+        <h1 class="mt-5 text-center">{{ $project->title }}</h1>
+        <p class="mt-5 text-center">{{ $project->body }}</p>
+    </div>
+</div>
+
+@endsection
